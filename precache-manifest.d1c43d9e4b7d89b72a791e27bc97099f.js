54052992ecbc4bb4ff9e1e07098218c4d10d9555
Dmitriy <danny88100@gmail.com>
2020-08-02 17:31:52 -0400

    Updates

---
+++ precache-manifest.d1c43d9e4b7d89b72a791e27bc97099f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462ce09c345c740cc140fa5443893dd1",
+    "url": "/deploy-crud/index.html"
+  },
+  {
+    "revision": "79a39ba525a953b0afb3",
+    "url": "/deploy-crud/static/css/2.d98961f1.chunk.css"
+  },
+  {
+    "revision": "084cdeb19e2a6792705c",
+    "url": "/deploy-crud/static/css/main.a144baaf.chunk.css"
+  },
+  {
+    "revision": "79a39ba525a953b0afb3",
+    "url": "/deploy-crud/static/js/2.e8da6038.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/deploy-crud/static/js/2.e8da6038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "084cdeb19e2a6792705c",
+    "url": "/deploy-crud/static/js/main.f3f60290.chunk.js"
+  },
+  {
+    "revision": "26bac18b13091a86265e",
+    "url": "/deploy-crud/static/js/runtime-main.13806ef6.js"
+  }
+]);
